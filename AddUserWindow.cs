adf408c7cc2d284f5fa6ffc3c67ee1d943f60b0c
Hiruna Fernando <Hiruna Fernando@DESKTOP-1U7JNSQ>
2023-07-16 22:20:26 +0530

    Add project files.

---
+++ AddUserWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace H3932
+
+{
+    /// <summary>
+    /// Interaction logic for AddUserWindow.xaml
+    /// </summary>
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow(AddUserWindow vm, Action closeAction)
+
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.CloseAction = () => Close();
+            CloseAction = closeAction;
+        }
+
+        public Action CloseAction { get; }
+    }
+}
---
+++ ImageFolder/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace H3932
+
+{
+    public class User
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public BitmapImage Image { get; set; }  
+
+        public string  DateOfBirth{ get; set; }
+        public Double GPA { get; set; }
+
+        public String ImagePath
+        {
+            get { return $"/ImageFolder/Images"; }
+        }
+
+        public User(int age, string firstName, string lastName, string dateOfBirth,BitmapImage  image )
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Image = image;
+        }
+
+        public User()
+        {
+        }
+    }
+}
